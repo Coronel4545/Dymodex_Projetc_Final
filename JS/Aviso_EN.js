139c2f910e0824c57b378c060f75ccb61458dd6d
Coronel4545 <stifler.oliveira41@gmail.com>
2022-11-01 21:07:41 -0300

    Update Aviso_EN.js

--- JS/Aviso_EN.js
+++ JS/Aviso_EN.js
@@ -1,4 +1,4 @@
-var Aviso = 'This is a purely explanatory page about the Dymodex project. $DMX!'
+var Aviso = 'This is a purely explanatory page about the Dymodex project. $DMX! (Non-responsive website)'
 
 		alert(Aviso)
 
